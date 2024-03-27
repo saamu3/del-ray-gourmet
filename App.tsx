99f510fe64d5740acb2b06555ed6080efdd30b39
Soumya <soumyakatta21@gmail.com>
2024-03-28 00:50:29 +0530

    import navigator from library and also import material top tab navigatore from library

--- App.tsx
+++ App.tsx
@@ -1,22 +1,33 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
-import Main from './components/Main';
+import * as React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Main from "./components/Main";
+import Section from "./components/Section";
 
+const Tab = createMaterialTopTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
-      <Main/>
+      <Main />
+      <Tab.Navigator style={{}}
+        // screenOptions={{
+        //   tabBarLabelStyle: { fontSize: 12,fontWeight:"bold"},
+        //   tabBarItemStyle: { width: 100 },
+        //   tabBarStyle: { backgroundColor: "#fff" },
+        // }}
+      >
+        <Tab.Screen name="Appetizers" component={Section} />
+        <Tab.Screen name="DESSERTS" component={Section} />
+        <Tab.Screen name="Main Entrees" component={Section} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
