973a30c810caf8b353b2598c677d7f90e037482b
Soumya <soumyakatta21@gmail.com>
2024-03-28 02:40:43 +0530

    delete unwanted content

--- StaticData.js/AppetizersData.tsx
+++ StaticData.js/AppetizersData.tsx
@@ -1,17 +1,3 @@
-// const Appetizers = [
-//   {
-//     id: "1",
-//     name: "DAY BOAT SCALLOPS",
-//     description: "fennel, prosciutto, black bean sauce",
-//     para: "Dietary Information",
-//   },
-//   {
-//     id: "2",
-//     name: "DEVILS ON HORSEBACK",
-//     description: "bacon wrapped dates stuffed with feta, balsamic reduction",
-//     para: "~ extra bacon $3",
-//   },
-// ];
 export const DATA = [
   {
     title: "APPETIZERS",
@@ -24,6 +10,7 @@ export const DATA = [
       },
       {
         name: "DEVILS ON HORSEBACK",
+        image:require("../assets/new.webp"),
         price: "$12",
         description:
           "bacon wrapped dates stuffed with feta, balsamic reduction",
@@ -32,58 +19,3 @@ export const DATA = [
     ],
   }
 ]
-//   {
-//     title: "MAIN ENTRÉES",
-//     data: [
-//       {
-//         name: "GRILLED FAROE ISLAND SALMON",
-//         image:"https://ik.imagekit.io/munchery/blog/tr:w-768/5-preparations-for-your-faroe-islands-farmed-salmon.jpeg",
-//         price: "$26",
-//         description: "quinoa, oyster mushrooms, brussels sprout leaves",
-//         para: "Dietary Information",
-//       },
-//       {
-//         name: "PAN ROASTED DUCK BREAST",
-//         price: "$29",
-//         description: "herbed farro, orange-frisée salad, honey gastrique",
-//         para: "Dietary Information",
-//       },
-//     ],
-//   },
-//   {
-//     title: "DESSERTS",
-//     data: [
-//       {
-//         name: "TRES LECHES CAKE",
-//         price: "$9",
-//         description:
-//           "Strawberry compote, strawberry balsamicquinoa, oyster mushrooms, brussels sprout leaves",
-//         para: "~ Jorge Ordonez & Co. Victoria 2 - Málaga, Spain $10.00 glass",
-//       },
-//       {
-//         name: "KEY LIME PUDDING",
-//         price: "$8",
-//         description: "herbed farro, orange-frisée salad, honey gastrique",
-//         para: "Dietary Information",
-//       },
-//     ],
-//   },
-// ];
-// export const DATA = [
-//     {
-//       title: 'Main dishes',
-//       data: [{ id:"1",name:'Pizza'}, { id:"2",name:'Burger'}, {id:"3",name:'Risotto'}],
-//     },
-    // {
-    //   title: 'Sides',
-    //   data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-    // },
-    // {
-    //   title: 'Drinks',
-    //   data: ['Water', 'Coke', 'Beer'],
-    // },
-    // {
-    //   title: 'Desserts',
-    //   data: ['Cheese Cake', 'Ice Cream'],
-    // },
-//   ];
