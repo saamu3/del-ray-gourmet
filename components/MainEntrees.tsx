7966c546af95a70952dca8335fd684a063609458
Soumya <soumyakatta21@gmail.com>
2024-03-28 01:06:02 +0530

    create mainEntrees component

---
+++ components/MainEntrees.tsx
@@ -0,0 +1,64 @@
+import { View,Text,SectionList,StyleSheet} from "react-native";
+import { Entrees } from "../StaticData.js/MainEntreesData";
+import { useState } from "react";
+import { SelectList } from "react-native-dropdown-select-list";
+export default function MainEntrees() {
+
+  return (
+    < View style={{marginTop:40}}>
+    <SectionList
+        sections={Entrees}
+        keyExtractor={(item: any, index: any) => item + index}
+        renderItem={({ item }: any) => (
+          <View style={styles.item}>
+            <View>
+              <Text style={styles.title}>{item.name}</Text>
+              <Text style={styles.title}>{item.description}</Text>
+              <Text style={styles.title}>{item.para}</Text>
+            </View>
+            <View>
+              <Text>{item.price}</Text>
+            </View>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1},
+  innerContainer: {
+    height: 30,
+    width: 360,
+    backgroundColor: "pink",
+    paddingLeft: 40,
+    paddingTop: 5,
+  },
+  text: { color: "white" },
+  address: { position: "absolute", color: "white" },
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 100,
+    width: 340,
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+  },
+  title: {
+    fontSize: 10,
+    color: "blue",
+  },
+  header: {
+    textAlign: "center",
+    fontStyle: "italic",
+    fontSize: 20,
+    color: "	#AA336A",
+    fontWeight: "bold",
+  },
+});
