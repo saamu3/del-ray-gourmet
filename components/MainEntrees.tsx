3c6465665c05ee7c52c350b6178ebfed29a99885
Soumya <soumyakatta21@gmail.com>
2024-03-28 04:30:17 +0530

    update the component by giving styling

--- components/MainEntrees.tsx
+++ components/MainEntrees.tsx
@@ -1,23 +1,66 @@
-import { View,Text,SectionList,StyleSheet} from "react-native";
+import { View, Text, SectionList, StyleSheet, Image } from "react-native";
 import { Entrees } from "../StaticData.js/MainEntreesData";
 import { useState } from "react";
 import { SelectList } from "react-native-dropdown-select-list";
 export default function MainEntrees() {
-
   return (
-    < View style={{marginTop:40}}>
-    <SectionList
+    <View style={{ marginTop: 40 }}>
+      <SectionList
         sections={Entrees}
         keyExtractor={(item: any, index: any) => item + index}
         renderItem={({ item }: any) => (
           <View style={styles.item}>
-            <View>
-              <Text style={{fontSize:18,color:"maroon"}}>{item.name}</Text>
-              <Text style={{fontSize:12,color:"firebrick",fontStyle:"italic"}}>{item.description}</Text>
-              <Text style={{fontSize:10,color:"tomato"}}>{item.para}</Text>
+            <View
+              style={{
+                height: 100,
+                width: 300,
+                // borderColor: "grey",
+                // borderWidth: 2,
+              }}
+            >
+              <View style={{ display: "flex", flexDirection: "row" }}>
+                <View>
+                  {item.image && (
+                    <Image
+                      source={item.image}
+                      style={{
+                        height: 90,
+                        width: 140,
+                        marginTop: 6,
+                        marginRight: 5,
+                        borderRadius: 5,
+                      }}
+                    />
+                  )}
+                </View>
+                <View style={{ flexShrink: 1 }}>
+                  <Text style={{ fontSize: 18, color: "maroon" }}>
+                    {item.name}
+                  </Text>
+                  {item.description && (
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        color: "firebrick",
+                        fontStyle: "italic",
+                        flexShrink: 1,
+                      }}
+                    >
+                      {item.description}
+                    </Text>
+                  )}
+
+                  <Text style={{ fontSize: 10, color: "tomato" }}>
+                    {item.para}
+                  </Text>
+                </View>
+              </View>
             </View>
+
             <View>
-              <Text style={{fontSize:15,color:"firebrick"}}>{item.price}</Text>
+              <Text style={{ fontSize: 15, color: "firebrick" }}>
+                {item.price}
+              </Text>
             </View>
           </View>
         )}
@@ -30,7 +73,7 @@ export default function MainEntrees() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1},
+  container: { flex: 1 },
   innerContainer: {
     height: 30,
     width: 360,
