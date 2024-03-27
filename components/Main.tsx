94bb1e48c2308eb72ea871db56b8cce2d18c947a
Soumya <soumyakatta21@gmail.com>
2024-03-28 02:43:00 +0530

    change the background image url

--- components/Main.tsx
+++ components/Main.tsx
@@ -20,7 +20,7 @@ const img1={
   uri:"https://qr.imenupro.com/cdn-cgi/image/width=320,format=auto,fit=scale-down/https://s3.imenupro.com/imp_oznkrLNkQdF9J3/4m-h9-qr2-logo.webp?v=231233669"
 }
 const image = {
-  uri: "https://media-cdn.tripadvisor.com/media/photo-s/27/ac/8b/8e/pretelt-gourmet-meats.jpg",
+  uri: "https://cdn-az.allevents.in/events5/banners/7d2aa1f19c32b002feb93c0a2a8502388c910aea17b6e438ec41f221d2be3e5c-rimg-w1200-h675-gmir.jpg?v=1709791272",
 };
 export default function Main() {
   return (
@@ -40,21 +40,21 @@ export default function Main() {
           source={image}
           style={{
             width: 400,
-            height: 200,
+            height: 400,
           }}
         > 
         <View style={{display:"flex",flexDirection:"row",justifyContent:"space-around"}}>
-         <Image source={img1} style={{height:100, width:150,marginTop:70}}/>
+         <Image source={img1} style={{height:100, width:180,marginTop:70}}/>
          <View style={styles.address}>
-          <Text style={{ color: "white" }}>Del Ray Gourmet</Text>
-          <Text style={{ color: "white" }}>
+          <Text style={{ color: "white",fontSize:25,fontWeight:"bold"}}>Del Ray Gourmet</Text>
+          <Text style={{ color: "white",fontSize:20,fontWeight:"bold"}}>
             Open • Closes 10:00 PM
             <FontAwesomeIcon style={{ color: "white" }} icon={faAngleDown} />
           </Text>
-          <Text style={{ color: "white" }}>
+          <Text style={{ color: "white",fontSize:15,fontWeight:"bold"}}>
             123 Ave of the Roosters, Derwood MD
           </Text>
-          <Text style={{ color: "white" }}>
+          <Text style={{ color: "white",fontSize:15,fontWeight:"bold" }}>
             <FontAwesomeIcon style={{ color: "white" }} icon={faPhone} />
             Tel: 301-555-1212
           </Text>
@@ -69,14 +69,14 @@ export default function Main() {
 const styles = StyleSheet.create({
   container: { flex: 1,backgroundColor:"skyblue"},
   innerContainer: {
-    height: 30,
+    height: 50,
     width: 360,
-    backgroundColor:"pink",
+    backgroundColor:"darkred",
     paddingLeft: 40,
-    paddingTop: 5,
+    paddingTop:7,
   },
   text: { color: "white" },
-  address: {marginTop:20,marginRight:30},
+  address: {marginTop:10,marginRight:40,gap:5},
   item: {
     display: "flex",
     flexDirection: "row",
