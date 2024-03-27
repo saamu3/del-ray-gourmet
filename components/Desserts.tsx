5e50b0f38df640d341f2d31908a6c8108d659bcd
Soumya <soumyakatta21@gmail.com>
2024-03-28 01:05:36 +0530

    rename navbar component name to dessets name

--- components/Desserts.tsx
+++ components/Desserts.tsx
@@ -1,37 +1,64 @@
-import { View, Text, StyleSheet,Pressable } from "react-native";
+import { View, Text, StyleSheet,SectionList } from "react-native";
+import { DessertsData } from "../StaticData.js/DessertsData";
 type TState = {
   visible:boolean,setIsVisible:React.Dispatch<React.SetStateAction<boolean>>
 }
-export default function Navbar() {
+export default function Desserts() {
   return (
-    <View style={styles.viewContainer}>
-      <Text style={styles.textContainer}>Appetizers</Text>
-      <Text style={styles.textContainer}>Main Entrees</Text>
-      <Text style={styles.textContainer}>Desserts</Text>
+    <View style={{marginTop:40}}>
+    <SectionList
+        sections={DessertsData}
+        keyExtractor={(item: any, index: any) => item + index}
+        renderItem={({ item }: any) => (
+          <View style={styles.item}>
+            <View>
+              <Text style={styles.title}>{item.name}</Text>
+              <Text style={styles.title}>{item.description}</Text>
+              <Text style={styles.title}>{item.para}</Text>
+            </View>
+            <View>
+              <Text>{item.price}</Text>
+            </View>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  viewContainer: {
+  container: { flex: 1},
+  innerContainer: {
+    height: 30,
+    width: 360,
+    backgroundColor: "pink",
+    paddingLeft: 40,
+    paddingTop: 5,
+  },
+  text: { color: "white" },
+  address: { position: "absolute", color: "white" },
+  item: {
     display: "flex",
     flexDirection: "row",
-    height: 40,
+    justifyContent: "space-between",
+    height: 100,
     width: 340,
-    borderColor: "grey",
-    borderWidth: 1,
-    gap: 40,
-    marginVertical: 20,
-    padding: 10,
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
   },
-  //   shadow:{
-  //     shadowColor: '#000',
-  //     shadowOffset: { width: 0, height: 1 },
-  //     shadowOpacity: 0.8,
-  //     shadowRadius: 2,
-  //     elevation: 5
-  //   },
-  textContainer: {
-    fontWeight: "600",
+  title: {
+    fontSize: 10,
+    color: "blue",
+  },
+  header: {
+    textAlign: "center",
+    fontStyle: "italic",
+    fontSize: 20,
+    color: "	#AA336A",
+    fontWeight: "bold",
   },
 });
