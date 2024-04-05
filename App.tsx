95d9120cdad7a22a0a0c4defa077f19d1ef96b83
Soumya <soumyakatta21@gmail.com>
2024-04-05 18:45:13 +0530

    rename the section component with appetizers

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Main from "./components/Main";
-import Section from "./components/Section";
 import Desserts from "./components/Desserts";
 import MainEntrees from "./components/MainEntrees";
+import Appetizers from "./components/Appetizers";
 
 const Tab = createMaterialTopTabNavigator();
 export default function App() {
@@ -19,7 +19,7 @@ export default function App() {
           tabBarPressColor: "lightpink",
         }}
       >
-        <Tab.Screen name="Appetizers" component={Section} />
+        <Tab.Screen name="Appetizers" component={Appetizers} />
         <Tab.Screen name="Main Entrées" component={MainEntrees} />
         <Tab.Screen name="Desserts" component={Desserts} />
       </Tab.Navigator>
---
+++ __Tests__/Appetizers.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Appetizers from "../components/Appetizers";
+import { render, screen } from "@testing-library/react-native";
+import "@testing-library/react-native/extend-expect";
+
+describe("should render Section component ", () => {
+  test("should check the image is present or not", () => {
+    render(<Appetizers />);
+    const image = screen.getByTestId("image1");
+    expect(image).toBeOnTheScreen();
+  });
+});
