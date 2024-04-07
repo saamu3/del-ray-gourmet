2dde504fefa8328b591faafa46a09e2d23c3211f
Soumya <soumyakatta21@gmail.com>
2024-04-07 15:06:31 +0530

    remove the inline styling

--- components/MainEntrees.tsx
+++ components/MainEntrees.tsx
@@ -8,65 +8,30 @@ export default function MainEntrees() {
         keyExtractor={(item: any, index: any) => item + index}
         renderItem={({ item }: any) => (
           <View style={styles.item}>
-            <View
-              style={{
-                // height: 100,
-                width: 300,
-                // borderColor: "grey",
-                // borderWidth: 2,
-              }}
-            >
-              <View style={{ display: "flex", flexDirection: "row" }}>
+            <View style={{ width: 300 }}>
+              <View style={styles.align}>
                 <View>
                   {item.image && (
                     <Image
                       testID="image3"
                       source={item.image}
-                      style={{
-                        height: 90,
-                        width: 140,
-                        marginTop: 6,
-                        marginRight: 15,
-                        borderRadius: 5,
-                      }}
+                      style={styles.image}
                     />
                   )}
                 </View>
                 <View style={{ flexShrink: 1 }}>
-                  <Text
-                    style={{
-                      fontSize: 18,
-                      color: "rgb(133 77 14)",
-                      fontFamily: "serif",
-                    }}
-                  >
-                    {item.name}
-                  </Text>
+                  <Text style={styles.itemName}>{item.name}</Text>
                   {item.description && (
-                    <Text
-                      style={{
-                        fontSize: 12,
-                        color: "firebrick",
-                        fontStyle: "italic",
-                        fontFamily: "serif",
-                        flexShrink: 1,
-                      }}
-                    >
-                      {item.description}
-                    </Text>
+                    <Text style={styles.description}>{item.description}</Text>
                   )}
 
-                  <Text style={{ fontSize: 10, color: "rgb(107 114 128)" }}>
-                    {item.para}
-                  </Text>
+                  <Text style={styles.para}>{item.para}</Text>
                 </View>
               </View>
             </View>
 
             <View>
-              <Text style={{ fontSize: 15, color: "rgb(133 77 14)" }}>
-                {item.price}
-              </Text>
+              <Text style={styles.price}>{item.price}</Text>
             </View>
           </View>
         )}
@@ -79,34 +44,36 @@ export default function MainEntrees() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  innerContainer: {
-    height: 30,
-    width: 360,
-    backgroundColor: "pink",
-    paddingLeft: 40,
-    paddingTop: 5,
-  },
-  text: { color: "white" },
-  address: { position: "absolute", color: "white" },
   item: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    // alignItems:"center",
-    //  height: 100,
-    // width: 340,
-    // backgroundColor: "#f9c2ff",
     padding: 20,
-    // marginVertical: 4,
   },
-  title: {
-    fontSize: 10,
-    color: "blue",
+  align: { display: "flex", flexDirection: "row" },
+  image: {
+    height: 90,
+    width: 140,
+    marginTop: 6,
+    marginRight: 15,
+    borderRadius: 5,
   },
+  itemName: {
+    fontSize: 18,
+    color: "rgb(133 77 14)",
+    fontFamily: "serif",
+  },
+  description: {
+    fontSize: 12,
+    color: "firebrick",
+    fontStyle: "italic",
+    fontFamily: "serif",
+    flexShrink: 1,
+  },
+  para: { fontSize: 10, color: "rgb(107 114 128)" },
+  price: { fontSize: 15, color: "rgb(133 77 14)" },
   header: {
     textAlign: "center",
-    // fontStyle: "italic",
     fontFamily: "serif",
     fontSize: 20,
     color: "rgb(248, 113, 113)",
