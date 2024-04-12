662bc0f4d69e2156c344391beaa2e630f03d6bd9
Soumya <soumyakatta21@gmail.com>
2024-04-12 14:23:26 +0530

    give styling to the text component

--- components/Main.tsx
+++ components/Main.tsx
@@ -12,6 +12,7 @@ import {
   View,
   ImageBackground,
 } from "react-native";
+import { TSpan } from "react-native-svg";
 const Del_Ray_gourmet_logo = {
   uri: "https://qr.imenupro.com/cdn-cgi/image/width=320,format=auto,fit=scale-down/https://s3.imenupro.com/imp_oznkrLNkQdF9J3/4m-h9-qr2-logo.webp?v=231233669",
 };
@@ -37,7 +38,7 @@ export default function Main(): React.JSX.Element {
             <View style={styles.address}>
               <Text style={styles.heading}>Del Ray Gourmet</Text>
               <Text style={styles.timings}>
-                Open • Closes 10:00 PM
+                <Text style={styles.open}>Open • </Text>Closes 10:00 PM
                 <FontAwesomeIcon style={styles.text} icon={faAngleDown} />
               </Text>
               <Text style={styles.place}>
@@ -77,6 +78,7 @@ const styles = StyleSheet.create({
   image: { height: 50, width: 80, marginTop: 90, marginLeft: 20 },
   heading: { color: "rgb(255 255 255)", fontSize: 25, fontWeight: "bold" },
   timings: { color: "white", fontSize: 17, fontWeight: "600" },
+  open:{color:"white",fontSize:20,fontWeight:"bold"},
   place: { color: "white", fontSize: 12, fontWeight: "400" },
   telephoneNumber: { color: "white", fontSize: 15, fontWeight: "400" },
   text: { color: "white" },
