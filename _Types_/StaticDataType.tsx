1a70261841653227acfa5fdc6869dc140bceac6d
Soumya <soumyakatta21@gmail.com>
2024-04-12 17:15:33 +0530

    create a file to keep the type of static data

---
+++ _Types_/StaticDataType.tsx
@@ -0,0 +1,10 @@
+export type TMenuItems = {
+    title: string;
+    data: {
+      name: string;
+      image?: string;
+      price: string;
+      description?: string;
+      para?: string;
+    }[];
+  };
