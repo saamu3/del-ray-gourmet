533dedf29515254c08738a260891a33a10069804
Soumya <soumyakatta21@gmail.com>
2024-04-13 18:07:29 +0530

    update the test

--- __Tests__/Appetizers.test.tsx
+++ __Tests__/Appetizers.test.tsx
@@ -3,8 +3,8 @@ import Appetizers from "../components/Appetizers";
 import { render, screen } from "@testing-library/react-native";
 import "@testing-library/react-native/extend-expect";
 
-describe("should render Section component ", () => {
-  test("should check the image is present or not", () => {
+describe("should render appetizers component ", () => {
+  test("should check the sticker is present or not", () => {
     render(<Appetizers />);
     const image = screen.getByTestId("sticker")
     screen.debug()
