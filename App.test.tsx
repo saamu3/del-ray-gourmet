afd22a68e774c6dd5f91052c48f3c33592eda68c
Soumya <soumyakatta21@gmail.com>
2024-04-16 12:09:38 +0530

    update the component

--- App.test.tsx
+++ App.test.tsx
@@ -1,8 +1,7 @@
+import { render, screen } from "@testing-library/react-native";
+import '@testing-library/react-native/extend-expect';
 import React from "react";
 import App from "./App";
-import MainEntrees from "./components/MainEntrees";
-import { render, screen} from "@testing-library/react-native";
-import '@testing-library/react-native/extend-expect';
 
 describe("should render the MainEntrees component",()=>{
     test("should check the image is present otr not",()=>{
