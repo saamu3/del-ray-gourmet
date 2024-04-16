480521ca2958ab8c29181ace75424fff8a5186ce
Soumya <soumyakatta21@gmail.com>
2024-04-16 12:08:26 +0530

    remove the stylesheets from the components and keep in the style file, import style from style file

--- components/Main.tsx
+++ components/Main.tsx
@@ -4,15 +4,8 @@ import {
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import {
-  Image,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-} from "react-native";
-
+import { Image, StatusBar, Text, View, ImageBackground } from "react-native";
+import style from "../styles/style";
 const Del_Ray_gourmet_logo = {
   uri: "https://qr.imenupro.com/cdn-cgi/image/width=320,format=auto,fit=scale-down/https://s3.imenupro.com/imp_oznkrLNkQdF9J3/4m-h9-qr2-logo.webp?v=231233669",
 };
@@ -21,32 +14,32 @@ export default function Main(): React.JSX.Element {
   return (
     <View>
       <StatusBar hidden />
-      <View style={styles.innerContainer}>
-        <Text style={styles.text}>
-          <FontAwesomeIcon style={styles.text} icon={faCircleExclamation} />
+      <View style={style.innerContainer}>
+        <Text style={style.text}>
+          <FontAwesomeIcon style={style.text} icon={faCircleExclamation} />
           WE ARE NOW OPEN FOR INDOOR DINING!
         </Text>
       </View>
-      <View style={styles.imageContainer}>
+      <View style={style.imageContainer}>
         <ImageBackground
           testID="background"
           source={require("../assets/Background.jpg")}
-          style={styles.imageBackground}
+          style={style.imageBackground}
           resizeMode="cover"
         >
-          <View style={styles.container}>
-            <Image source={Del_Ray_gourmet_logo} style={styles.image} />
-            <View style={styles.address}>
-              <Text style={styles.heading}>Del Ray Gourmet</Text>
-              <Text style={styles.timings}>
-                <Text style={styles.open}>Open • </Text>Closes 9:00 PM
-                <FontAwesomeIcon style={styles.text} icon={faAngleDown} />
+          <View style={style.container}>
+            <Image source={Del_Ray_gourmet_logo} style={style.image} />
+            <View style={style.address}>
+              <Text style={style.heading}>Del Ray Gourmet</Text>
+              <Text style={style.timings}>
+                <Text style={style.open}>Open • </Text>Closes 9:00 PM
+                <FontAwesomeIcon style={style.text} icon={faAngleDown} />
               </Text>
-              <Text style={styles.place}>
+              <Text style={style.place}>
                 123 Ave of the Roosters, Derwood MD
               </Text>
-              <Text style={styles.telephoneNumber}>
-                <FontAwesomeIcon style={styles.text} icon={faPhone} />
+              <Text style={style.telephoneNumber}>
+                <FontAwesomeIcon style={style.text} icon={faPhone} />
                 Tel: 301-555-1212
               </Text>
             </View>
@@ -56,34 +49,3 @@ export default function Main(): React.JSX.Element {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  innerContainer: {
-    height: 50,
-    // width: 360,
-    backgroundColor: "rgb(248 113 113)",
-    justifyContent:"center",
-    alignItems:"center",
-    // paddingLeft: 40,
-    // paddingTop: 1,
-  },
-  imageContainer: { justifyContent: "center", alignItems: "center" },
-  imageBackground: {
-    width: "100%",
-    height: 200,
-    opacity: 0.99,
-  },
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  image: { height: 50, width: 80, marginTop: 90, marginLeft: 20 },
-  heading: { color: "rgb(255 255 255)", fontSize: 20, fontWeight: "bold" },
-  timings: { color: "white", fontSize: 13, fontWeight: "500" },
-  open: { color: "white", fontSize: 18, fontWeight: "bold" },
-  place: { color: "white", fontSize: 12, fontWeight: "400" },
-  telephoneNumber: { color: "white", fontSize: 15, fontWeight: "400" },
-  text: { color: "white" },
-  address: { marginTop: 70, marginRight: 95, flexShrink: 1 },
-});
--- components/Sections.tsx
+++ components/Sections.tsx
@@ -5,6 +5,7 @@ import {
   View,
   useWindowDimensions,
 } from "react-native";
+import style from "../styles/style";
 import { TMenuItems } from "../_Types_/StaticDataType";
 import SectionList from "react-native-tabs-section-list/lib/SectionList";
 export default function Sections({
@@ -15,27 +16,27 @@ export default function Sections({
   const windowWidth = useWindowDimensions().width;
   const windowHeight = useWindowDimensions().height;
   return (
-    <View style={styles.container}>
+    <View style={style.sectionContainer}>
       <SectionList
         sections={Data}
         keyExtractor={(item) => item.title}
         stickySectionHeadersEnabled={false}
         scrollToLocationOffset={10}
-        tabBarStyle={styles.tabBar}
-        ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
+        tabBarStyle={style.tabBar}
+        ItemSeparatorComponent={() => <View style={style.itemSeparator} />}
         SectionSeparatorComponent={() => (
-          <View style={styles.sectionSeparator} />
+          <View style={style.sectionSeparator} />
         )}
         renderTab={({ title, isActive }) => (
           <View
             style={[
-              styles.tabContainer,
+              style.tabContainer,
               { borderBottomWidth: isActive ? 2 : 0 },
             ]}
           >
             <Text
               style={[
-                styles.tabText,
+                style.tabText,
                 { color: isActive ? "rgb(248, 113, 113)" : "black" },
               ]}
             >
@@ -44,16 +45,16 @@ export default function Sections({
           </View>
         )}
         renderSectionHeader={({ section: { title } }) => (
-          <Text style={styles.header}>{title}</Text>
+          <Text style={style.header}>{title}</Text>
         )}
         renderItem={({ item }) => (
-          <View style={styles.item}>
+          <View style={style.item}>
             {item.image && (
               <Image
                 testID="Appetizers"
                 source={item.image}
                 style={[
-                  styles.image,
+                  style.itemImage,
                   {
                     height: windowHeight > 700 ? "100%" : "50%",
                     width: windowWidth > 500 ? "70%" : "50%",
@@ -64,28 +65,28 @@ export default function Sections({
             )}
             {item.sticker && (
               <Image
-                style={styles.sticker}
+                style={style.sticker}
                 source={item.sticker}
                 testID="sticker"
               />
             )}
 
             <View style={{ flexShrink: 1 }}>
-              <Text style={styles.itemName}>{item.name}</Text>
+              <Text style={style.itemName}>{item.name}</Text>
               {item.description && (
-                <Text style={styles.description}>{item.description}</Text>
+                <Text style={style.description}>{item.description}</Text>
               )}
               {item.additional_information && (
-                <Text style={styles.additional_information}>
+                <Text style={style.additional_information}>
                   {item.additional_information}
                 </Text>
               )}
-              <Text style={styles.additional_items}>
+              <Text style={style.additional_items}>
                 {item.additional_items}
               </Text>
             </View>
             <View>
-              <Text style={styles.price}>{item.price}</Text>
+              <Text style={style.price}>{item.price}</Text>
             </View>
           </View>
         )}
@@ -93,77 +94,3 @@ export default function Sections({
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f6f6f6",
-  },
-  tabBar: {
-    backgroundColor: "#fff",
-    borderBottomColor: "#f4f4f4",
-    borderBottomWidth: 1,
-  },
-  tabContainer: {
-    borderBottomColor: "rgb(248, 113, 113)",
-  },
-  tabText: {
-    padding: 15,
-    fontSize: 15,
-    fontWeight: "500",
-  },
-  itemSeparator: {
-    height: 0.5,
-    width: "96%",
-  },
-  sectionSeparator: {
-    marginBottom: 5,
-    height: 0.5,
-    width: "96%",
-    alignSelf: "flex-start",
-    backgroundColor: "",
-    marginVertical: 10,
-  },
-  item: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 10,
-  },
-  image: {
-    marginTop: 5,
-    marginRight: 10,
-    borderRadius: 5,
-    flexShrink: 1,
-  },
-  align: { display: "flex", flexDirection: "row", flexShrink: 1 },
-  sticker: { height: 10, width: 20, marginTop: 10 },
-  itemName: {
-    fontSize: 18,
-    color: "rgb(133 77 14)",
-    fontFamily: "serif",
-  },
-  description: {
-    fontSize: 12,
-    color: "black",
-    fontStyle: "italic",
-    fontFamily: "serif",
-  },
-  additional_information: {
-    textDecorationLine: "underline",
-    textDecorationStyle: "solid",
-    textDecorationColor: "#000",
-    fontSize: 10,
-    color: "rgb(107 114 128)",
-  },
-  additional_items: { fontSize: 10, color: "rgb(107 114 128)" },
-  price: { fontSize: 15, color: "rgb(133 77 14)" },
-  header: {
-    marginTop: 20,
-    textAlign: "center",
-    fontFamily: "serif",
-    fontSize: 20,
-    color: "rgb(248, 113, 113)",
-    fontWeight: "bold",
-  },
-});
