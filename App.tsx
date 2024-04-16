6bd36975d8229fe193f5ecd0521c1e6472a87851
Soumya <soumyakatta21@gmail.com>
2024-04-16 12:58:16 +0530

    update component by using of safeareaview and platform ccomponents

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import { SectionsData } from "./StaticData/SectionsData";
 import Main from "./components/Main";
 import Sections from "./components/Sections";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Main />
       <Sections Data={SectionsData} />
-    </View>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1,paddingTop:Platform.OS==="android"?0:StatusBar.currentHeight},
 });
