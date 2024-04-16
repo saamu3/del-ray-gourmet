c25a6f872beccbadf31bbac96a03d35c056d8206
Soumya <soumyakatta21@gmail.com>
2024-04-16 11:32:15 +0530

    delete the components

--- App.tsx
+++ App.tsx
@@ -1,31 +1,14 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
-import Appetizers from "./components/Appetizers";
-import Desserts from "./components/Desserts";
+import { SectionsData } from "./StaticData/SectionsData";
 import Main from "./components/Main";
-import MainEntrees from "./components/MainEntrees";
+import Sections from "./components/Sections";
 
-const Tab = createMaterialTopTabNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationContainer>
-        <Main />
-        <Tab.Navigator
-          screenOptions={{
-            tabBarLabelStyle: { fontSize: 12, fontWeight: "500" },
-            tabBarActiveTintColor: "rgb(248, 113, 113)",
-            tabBarInactiveTintColor: "black",
-            tabBarPressColor: "lightpink",
-          }}
-        >
-          <Tab.Screen name="Appetizers" component={Appetizers} />
-          <Tab.Screen name="Main Entrées" component={MainEntrees} />
-          <Tab.Screen name="Desserts" component={Desserts} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Main />
+      <Sections Data={SectionsData} />
     </View>
   );
 }
